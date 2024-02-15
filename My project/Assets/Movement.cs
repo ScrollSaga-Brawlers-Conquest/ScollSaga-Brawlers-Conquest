f5ae7a45c15e6ddd4f49c1956c67d17ad63758a5
antithesis224 <colby224@comcast.net>
2024-02-15 16:26:41 -0500

    jump now works and has a delay

--- My project/Assets/Movement.cs
+++ My project/Assets/Movement.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2 jumpHeight;
+    private bool isCoolDown = false;
+    private float coolDown = 1f;
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector2.left * Time.deltaTime);
@@ -23,44 +26,20 @@ public class Movement : MonoBehaviour
         {
             transform.Translate(Vector2.right * Time.deltaTime);
         }
-    }
-}
 
-public class GroundCheck : MonoBehaviour
-{
-    public float distanceToCheck = 0.5f;
-    public bool isGrounded;
-    private void Update()
-    {
-        if (Physics2D.Raycast(transform.position, Vector2.down, distanceToCheck))
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-    }
-}
-
-public class Jump : MonoBehaviour
-{
-    public GroundCheck groundCheck;
-    public float jumpForce = 20;
-    public float gravity = -9.81f;
-    public float gravityScale = 5;
-    float velocity;
-    void Update()
-    {
-        velocity += gravity * gravityScale * Time.deltaTime;
-        if (groundCheck.isGrounded && velocity < 0)
-        {
-            velocity = 0;
-        }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            velocity = jumpForce;
+            if (isCoolDown == false)
+            {
+                GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+                StartCoroutine(CoolDown());
+            }
         }
-        transform.Translate(new Vector2(0, velocity) * Time.deltaTime);
     }
+    IEnumerator CoolDown()
+    {
+        isCoolDown = true;
+        yield return new WaitForSeconds(coolDown);
+        isCoolDown = false;
+    }      
 }
